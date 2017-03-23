3a8f585d376b20bda3dbbc8bccee808d548d021f
ahorod <prozerpina112009@gmail.com>
2017-03-22 22:33:26 -0700

    Add product declare functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,20 @@
-// $(document).ready(function() {
-//   $("#form").submit(function(){
-//     var nameInput = $("input#name").val();
-//     $(".name").text(nameInput);
-//
-//     $("#story").show();
-//
-//     event.preventDefault();
-//   });
-// });
 $(document).ready(function() {
-  $("#btn1").click(function(event){
-    event.preventDefault();
-    $("#form").show();
-        $("#submit").click(function(event){
-          var nameInput = $("input#name").val();
-          var addressInput = $("input#address").val();
-          $("#receipt").text(name);
-          event.preventDefault();
-        });
-  });
+  function registerProduct(button, productName) {
+    $("#"+button).click(function(event){
+      event.preventDefault();
+      $("#form").show();
+      $("#form").unbind().submit(function(event){
+        var name = $("#name").val();
+        var address = $("#address").val();
+        $("#form").hide();
+        $("#name").val("");
+        $("#address").val("");
+        $("#receipt").append(productName + name );
+        event.preventDefault();
+      });
+    });
+  }
+  registerProduct("btn-flour","Flour");
+  registerProduct("btn-chocolate","Chocolate");
+  registerProduct("btn-tools","Tools");
 });
